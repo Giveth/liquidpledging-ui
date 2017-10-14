1693e0f4f0bffe2b01515ffd7ab66de06499d31b
Xavi <xavings@gmail.com>
2017-10-14 14:41:30 +0200

    Wrapper renamed to LiquidPledgingController

---
+++ src/LiquidPledgingController.js
@@ -0,0 +1,45 @@
+import ProviderHelper from "./Web3ProviderHelper"
+
+const EventEmitter = require('events')
+const httpProvider = 'http://localhost:8545'
+
+
+class LiquidPledgingController extends EventEmitter {
+
+    constructor()
+    {
+        super()
+        this.data = []
+        this.interval = {}
+        this.startInterval()
+        this.STATE_CHANGED = "stateChanged"
+        this.setupWeb3()
+    }
+
+    startInterval()
+    {
+        this.interval=setInterval(() => {
+            for (let i=0; i<10; i += 1) {
+                this.data[i] = Math.floor(Math.random() * 10);
+            }
+
+            this.emit(this.STATE_CHANGED)
+
+        }, 1000)
+    }
+
+    setupWeb3()
+    {
+        ProviderHelper.setup(["*"]).then(()=>{
+
+            }).catch((e)=>{console.error(e)})
+        }
+
+    getData()
+    {
+        return this.data
+    }
+  
+}
+
+export default new LiquidPledgingController()
