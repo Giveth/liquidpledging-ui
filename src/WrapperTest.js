8398bd295d21c3a082ccb6935d7e59f0766773d8
Xavi <xavings@gmail.com>
2017-10-14 15:24:45 +0200

    Adding networkChange listener

--- src/WrapperTest.js
+++ src/WrapperTest.js
@@ -6,26 +6,40 @@ class WrapperTest extends Component {
     constructor(){
         super()
 
+        this.state={
+            data:[],
+            network:"",
+            account:""
+        }
+
         Wrapper.on(Wrapper.STATE_CHANGED, this.onStateChanged)
         Wrapper.on(Wrapper.ACCOUNT_CHANGED, this.onAccountChanged)
-        this.state={data:[1,2,3]}
+        Wrapper.on(Wrapper.NETWORK_CHANGED, this.onNetworkChanged)
     }
 
     onStateChanged=()=>{
-
-        let data = Wrapper.getData()
-        //this.setState({data:data})
+        console.log(Wrapper.getData())
+        let newData = Wrapper.getData().toString()
+        this.setState({data:newData})
     }
 
     onAccountChanged=()=>{
         let newAccount = Wrapper.getCurrentAccount()
-        this.setState({data:newAccount})
+        this.setState({account:newAccount})
+    }
+
+    onNetworkChanged=()=>{
+        let newNetwork = Wrapper.getCurrentNetwork().name
+        console.log(newNetwork)
+        this.setState({network:newNetwork})
     }
 
     render() {
         return (
             <div >
-                <h1 style ={{wordWrap: 'break-word'}}> {this.state.data.toString()} </h1>
+                <h1 key = {"data"} style ={{wordWrap: 'break-word'}}> {this.state.data} </h1>
+                <h1 key = {"account"} style ={{wordWrap: 'break-word'}}> {this.state.account} </h1>
+                <h1 key = {"network"} style ={{wordWrap: 'break-word'}}> {this.state.network} </h1>
             </div>
         )
     }
