cf5d305116b9cbe0302843a948851b95de8f2f99
Xavi <xavings@gmail.com>
2017-10-15 17:10:52 +0200

    Extending LiquidPledgingController to abstract the api calls to the state

---
+++ src/LiquidPledgingState.js
@@ -0,0 +1,13 @@
+import LiquidPledgingController from "./LiquidPledgingController"
+
+class LiquidPledgingState extends LiquidPledgingController {
+
+    constructor()
+    {
+        super()
+    }
+
+   
+}
+
+export default new LiquidPledgingState ()
