af116daf1ec26b7c76dbe4e12f1cb40b8c6eb1d7
Xavi <xavings@gmail.com>
2017-10-17 10:49:32 +0200

    Changing last delegate user and renaming of usernames and urls

--- scripts/basicScenario.js
+++ scripts/basicScenario.js
@@ -9,18 +9,18 @@ const LiquidPledgingState = liquidpledging.LiquidPledgingState;
 
 web3 = new Web3("ws://localhost:8546");
 
-let giver1;
-let delegate1;
-let delegate2;
-let adminProject1;
+let user1;
+let user2;
+let user3;
+let user4;
 
 async function run() {
     accounts = await web3.eth.getAccounts();
 
-    giver1 = accounts[0];
-    delegate1 = accounts[1];
-    adminProject1 = accounts[2];
-    delegate2 = accounts[3];
+    user1 = accounts[0];
+    user2 = accounts[1];
+    user4 = accounts[2];
+    user3 = accounts[3];
 
     vault = await Vault.new(web3);
     liquidPledging = await LiquidPledging.new(web3, vault.$address, { gas: 5800000 });
@@ -29,39 +29,40 @@ async function run() {
 
 // Create a donor
 
-    await liquidPledging.addGiver('Giver1', 'URLGiver1', 600, 0, { from: giver1 });
+    await liquidPledging.addGiver('Giver1User1', 'Giver1User1', 600, 0, { from: user1 });
 
 // Donate
 // Admin #1 donates 2 ether to himself
 // A pledge is created as a result (#1)
 
-    await liquidPledging.donate(1, 1, { from: giver1, value: utils.toWei(2) });
+    await liquidPledging.donate(1, 1, { from: user1, value: utils.toWei(2) });
 
 // Create a delegate
-    await liquidPledging.addDelegate('Delegate1', 'URLDelegate1', 0, 0, { from: delegate1 });
+    await liquidPledging.addDelegate('Delegate1User2', 'URLDelegate1User2', 0, 0, { from: user2 });
 
 // Create a delegate
-    await liquidPledging.addDelegate('Delegate2', 'URLDelegate2', 0, 0, { from: delegate2 });
-
+    await liquidPledging.addDelegate('Delegate1User3', 'URLDelegate1User3', 0, 0, { from: user3 });
 
 // Delegate
 // Admin#1 delegates 0.5 ether to admin#2
 // Pledge #2 is created as a result
-    await liquidPledging.transfer(1, 1, utils.toWei(0.5), 2, { from: giver1 });
+    await liquidPledging.transfer(1, 1, utils.toWei(0.5), 2, { from: user1 });
 
 // Delegate
 // Admin #2 delegates 0.5 ether to admin #3
 // Pledge #2 is created as a result
-    await liquidPledging.transfer(2, 2, utils.toWei(0.2), 3, { from: delegate1 });
-
+    await liquidPledging.transfer(2, 2, utils.toWei(0.2), 3, { from: user2 });
 
 // Create a project
-    await liquidPledging.addProject('Project1', 'URLProject1', adminProject1, 0, 86400, 0, { from: adminProject1 });
-
+    await liquidPledging.addProject('Project1User4', 'URLProject1User4', user4, 0, 86400, 0, { from: user4 });
 
 // Propose a project
 // Admin #2 delegates to pledge#2 0.2 eth
-    await liquidPledging.transfer(2, 3, utils.toWei(0.2), 4, { from: delegate1 });
+    await liquidPledging.transfer(2, 3, utils.toWei(0.2), 4, { from: user2 });
+
+// Create a delegate
+    await liquidPledging.addDelegate('Delegate1User1', 'URLDelegate1User', 0, 0, { from: user1 });
+
 
     const st = await liquidPledgingState.getState();
     console.log(JSON.stringify(st, null, 2));
