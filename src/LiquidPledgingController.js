20010136d4b8516b7916a65687fbc4621cbcc091
Xavi <xavings@gmail.com>
2017-10-18 17:31:43 +0200

    Removing fake data

--- src/LiquidPledgingController.js
+++ src/LiquidPledgingController.js
@@ -55,9 +55,7 @@ class LiquidPledgingController extends ProviderHelper {
     {
         this.liquidPledgingState.getState()
             .then((data) => {
-
-                this.setState(data)
-                //this.setFakeState()
+                this.setFakeState()
             })
             .catch((error)=>{console.error(error)})
     }
@@ -71,6 +69,7 @@ class LiquidPledgingController extends ProviderHelper {
             this.state.pledges=this.setIds(this.state.pledges)
             this.state.pledges.shift() //first item is always null
             this.state.pledges=this.setStringsToNumbers(this.state.pledges)
+            this.delegations = this.createDelegations(this.state.pledges)
         }
         else
             this.state.pledges=[]
@@ -88,13 +87,6 @@ class LiquidPledgingController extends ProviderHelper {
 
     getState(){
         return this.state
-    }
-
-    //TODO: Remove this function and 'fakeState' const
-    setFakeState()
-    {
-        this.state = fakeState
-        this.emit(this.STATE_CHANGED)
     }
 
     setIds(array)
@@ -126,119 +118,11 @@ class LiquidPledgingController extends ProviderHelper {
             
         })
     }
+
+    createDelegations(pledges)
+    {}
 }
 
-const fakeState = {
-  "pledges": [
-    null,
-    {
-      "delegates": [],
-      "amount": "1500000000000000000",
-      "owner": "1",
-      "intendedProject": "0",
-      "commmitTime": "0",
-      "oldPledge": "0",
-      "paymentState": "Pledged"
-    },
-    {
-      "delegates": [
-        {
-          "id": "2",
-          "addr": "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
-          "name": "Delegate1User2"
-        }
-      ],
-      "amount": "300000000000000000",
-      "owner": "1",
-      "intendedProject": "0",
-      "commmitTime": "0",
-      "oldPledge": "0",
-      "paymentState": "Pledged"
-    },
-    {
-      "delegates": [
-        {
-          "id": "2",
-          "addr": "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
-          "name": "Delegate1User2"
-        },
-        {
-          "id": "3",
-          "addr": "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d",
-          "name": "Delegate1User3"
-        }
-      ],
-      "amount": "0",
-      "owner": "1",
-      "intendedProject": "0",
-      "commmitTime": "0",
-      "oldPledge": "0",
-      "paymentState": "Pledged"
-    },
-    {
-      "delegates": [
-        {
-          "id": "2",
-          "addr": "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
-          "name": "Delegate1User2"
-        }
-      ],
-      "amount": "200000000000000000",
-      "owner": "1",
-      "intendedProject": "4",
-      "commmitTime": "1508230664",
-      "oldPledge": "0",
-      "paymentState": "Pledged"
-    }
-  ],
-  "admins": [
-    null,
-    {
-      "type": "Giver",
-      "addr": "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
-      "name": "Giver1User1",
-      "url": "Giver1User1",
-      "commitTime": "600",
-      "plugin": "0x0000000000000000000000000000000000000000",
-      "canceled": false
-    },
-    {
-      "type": "Delegate",
-      "addr": "0xFFcf8FDEE72ac11b5c542428B35EEF5769C409f0",
-      "name": "Delegate1User2",
-      "url": "URLDelegate1User2",
-      "commitTime": "0",
-      "plugin": "0x0000000000000000000000000000000000000000",
-      "canceled": false
-    },
-    {
-      "type": "Delegate",
-      "addr": "0xE11BA2b4D45Eaed5996Cd0823791E0C93114882d",
-      "name": "Delegate1User3",
-      "url": "URLDelegate1User3",
-      "commitTime": "0",
-      "plugin": "0x0000000000000000000000000000000000000000",
-      "canceled": false
-    },
-    {
-      "type": "Project",
-      "addr": "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
-      "name": "Project1User4",
-      "url": "URLProject1User4",
-      "commitTime": "86400",
-      "plugin": "0x0000000000000000000000000000000000000000",
-      "canceled": false
-    },
-    {
-      "type": "Delegate",
-      "addr": "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
-      "name": "Delegate1User1",
-      "url": "URLDelegate1User",
-      "commitTime": "0",
-      "plugin": "0x0000000000000000000000000000000000000000",
-      "canceled": false
-    }
-  ]
-}
+
 
 export default LiquidPledgingController
