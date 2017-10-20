f82273b4aeeae071e98ec12a947e342fe9a77abd
Xavi <xavings@gmail.com>
2017-10-21 00:23:59 +0200

    Setting up workflow for donate dialog

--- src/LiquidPledgingCaller.js
+++ src/LiquidPledgingCaller.js
@@ -1,24 +1,29 @@
 import LiquidPledging from './LiquidPledgingState'
 const EventEmitter = require('events')
 
-class LiquidPledgingCaller extends EventEmitter
+class Caller extends EventEmitter
 {
     constructor()
     {
         super()
+        this.DONATE_DIALOG = 'donateDialog'
     }
 
-    donate(giverId, receiverId, amount )
+    showDonateDialog(donateData)
     {
-       LiquidPledging.donate(giverId, receiverId, amount )
+        this.emit(this.DONATE_DIALOG, donateData)
+    }
+
+    donate(donateData)
+    {
+       LiquidPledging.donate(donateData.emiter, donateData.reciever, donateData.amount )
        .then((data) => {
-            
             console.log("Donated", data)
         })
     }
 
 }
 
-const instance = new LiquidPledgingCaller()
+const instance = new Caller()
 
 export default instance;
