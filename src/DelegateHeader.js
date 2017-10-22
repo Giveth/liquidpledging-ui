a1b2a431906184efea07aa5b99b85890cc6c4559
Xavi <xavings@gmail.com>
2017-10-23 01:44:54 +0200

    Not showing cancel button for now

--- src/DelegateHeader.js
+++ src/DelegateHeader.js
@@ -50,7 +50,7 @@ class DelegateHeader extends Component {
     render() {
 
         let isAdmin = (this.props.userAddress === this.props.delegation.adminAddress)
-        let canCancel = true
+        let canCancel = false
 
         let toggleIcon = <Icons.colapsed size={20}/>
         if(this.props.colapsed)
