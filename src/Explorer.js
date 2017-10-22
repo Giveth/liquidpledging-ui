6b7c2e07560f3a3a9369f5e9e5cd9d85978f2f29
Xavi <xavings@gmail.com>
2017-10-23 01:01:26 +0200

    Adding Explorer page

---
+++ src/Explorer.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import LPState from "./LiquidPledgingState.js"
+import DelegationsList from './DelegationsList'
+import {Styles, Merge} from './Styles'
+
+const title = "Explorer"
+
+class Explorer extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            network:"",
+            treeChildren:[]
+        }
+
+        LPState.on(LPState.STATE_CHANGED, this.onStateChanged)
+        LPState.on(LPState.ACCOUNT_CHANGED, this.onAccountChanged)
+        LPState.on(LPState.NETWORK_CHANGED, this.onNetworkChanged)
+    }
+
+    onStateChanged=()=>{
+        this.setDelegations()
+    }
+
+    onAccountChanged=()=>{
+        this.setDelegations()
+    }
+
+    onNetworkChanged=()=>{
+        this.setDelegations()
+    }
+
+    setDelegations=()=>
+    {
+        let address = LPState.getCurrentAccount()
+
+        if(!address)
+        {
+            this.setState({
+                treeChildren:[],
+                currentAddress:'Not connected to Ethereum...  (╯°□°）╯︵ ┻━┻',
+            })
+            return
+        }
+
+        let delegatesFilter = {type:'Delegate'}
+        let delegatesNodes =  LPState.getNodes(delegatesFilter)
+        let projectsFilter = {type:'Project'}
+        let projectNodes =  LPState.getNodes(projectsFilter)
+
+        let delegations = LPState.getFirstDelegationsForNodes(projectNodes.concat(delegatesNodes))
+        let onlyDelegationsWithMoneyFilter= {assignedAmount:undefined}
+        let myTrees = LPState.getDelegationsTrees(delegations, onlyDelegationsWithMoneyFilter)
+
+        this.setState({
+            treeChildren:myTrees,
+            currentAddress:address,
+        })
+    }
+
+    render() {
+
+        return (
+            <div >
+                <p key = {"title"} style ={Styles.subtitle}> {title} </p>
+                <p key = {"currentAddress"} style ={Merge(Styles.addressSubtle, Styles.adminColor)}> {this.state.currentAddress} </p>
+                <DelegationsList treeChildren={this.state.treeChildren} indentLevel={-1} userAddress={this.state.currentAddress}/>
+            </div>
+        )
+    }
+}
+
+export default Explorer
