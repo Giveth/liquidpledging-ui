5da7ac0e70af39992c59a896501e60b69c2ddf9a
Xavi <xavings@gmail.com>
2017-10-24 19:23:33 +0200

    Adding giver view

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Explorer from './Explorer'
 import {Tabs, Tab} from 'material-ui/Tabs'
 import LayoutController from './LayoutController'
 import PledgesView from './PledgesView'
+import GiverView from './GiverView'
 
 class App extends Component {
 
@@ -37,8 +38,8 @@ class App extends Component {
                                 style={Styles.givethLogo}/>
                         </div>
 
-                        <LayoutController defaultTab = {3}>
-
+                        <LayoutController defaultTab = {0}>
+                             <GiverView label="Giver"/>
                             <MyFunds label="My funds"/>
                             <OthersFunds label="Other's funds" />
                             <Explorer label="Explorer" />
---
+++ src/GiverView.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import LPState from "./LiquidPledgingState.js"
+import DelegationsList from './DelegationsList'
+import {Styles, Merge} from './Styles'
+
+const title = 'My funds'
+
+class GiverView extends Component {
+
+    constructor(){
+        super()
+
+        this.state={
+            network:"",
+            treeChildren:[]
+        }
+
+        LPState.on(LPState.STATE_CHANGED, this.onStateChanged)
+        LPState.on(LPState.ACCOUNT_CHANGED, this.onAccountChanged)
+        LPState.on(LPState.NETWORK_CHANGED, this.onNetworkChanged)
+    }
+
+    onStateChanged=()=>{
+        this.setDelegations()
+    }
+
+    onAccountChanged=()=>{
+        this.setDelegations()
+    }
+
+    onNetworkChanged=()=>{
+        this.setDelegations()
+    }
+
+    setDelegations=()=>
+    {
+        let address = LPState.getCurrentAccount()
+
+        if(!address)
+        {
+            this.setState({
+                treeChildren:[],
+                currentAddress:'Not connected to Ethereum...  (╯°□°）╯︵ ┻━┻',
+            })
+            return
+        }
+
+        let myGiversFilter = {address:address, type:'Giver'}
+        let myNodes =  LPState.getNodes(myGiversFilter)
+
+        let firstNode = myNodes[0]
+
+        let myDelegations = LPState.getFirstDelegationsForNodes(myNodes)
+        let onlyDelegationsWithMoneyFilter= {assignedAmount:undefined}
+        let myTrees = LPState.getDelegationsTrees(myDelegations, onlyDelegationsWithMoneyFilter)
+
+        this.setState({
+            treeChildren:myTrees,
+            currentAddress:address,
+        })
+    }
+
+    render() {
+
+        return (
+            <div >
+                <p key = {"currentAddress"} style ={Merge(Styles.addressSubtle, Styles.adminColor)}> {this.state.currentAddress} </p>
+                <DelegationsList
+                    treeChildren={this.state.treeChildren}
+                    indentLevel={-1}
+                    userAddress={this.state.currentAddress}
+                    defaultColapsed = {false}/>
+
+            </div>
+        )
+    }
+}
+
+export default GiverView
