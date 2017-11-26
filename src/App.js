68d0498128ab27645606efa9adc2bc3c64b4bcbb
Xavi <xavings@gmail.com>
2017-11-26 13:47:52 +0100

    App.js cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 import liquidPledgingLogo from './liquid-pledging.svg'
 import './App.css'
-
 import Page from './Page'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { Styles } from './Styles'
@@ -12,17 +10,11 @@ import MyFunds from './MyFunds'
 import AddAdmin from './AddAdmin'
 import OthersFunds from './OthersFunds'
 import Explorer from './Explorer'
-import {Tabs, Tab} from 'material-ui/Tabs'
 import LayoutController from './LayoutController'
 import PledgesView from './PledgesView'
 import AddressSelector from './AddressSelector'
 
 class App extends Component {
-
-    constructor()
-    {
-        super()
-    }
 
     render() {
         return (
@@ -33,7 +25,6 @@ class App extends Component {
                     
                     <Page>
 
-                       
                         <div style={{textAlign: 'center'}}>
                             <img
                                 src={liquidPledgingLogo}
