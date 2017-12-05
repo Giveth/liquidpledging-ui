91569f3d97bb67b25140700e231feaa089de74a7
Xavi <xavings@gmail.com>
2017-12-05 16:02:21 +0100

    Unnecessary code cleanup

--- src/RootDelegationList.js
+++ src/RootDelegationList.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react'
 import RootDelegation from './RootDelegation'
-import { Styles } from './Styles'
 
-class RootDelegationList extends Component {
-
-    constructor(props){
-        super()
-
-        this.state={}
-    }
-   
+class RootDelegationList extends Component {   
     render() {
-        let indentLevel = this.props.indentLevel + 1
+       
         let list = []
 
         if(this.props.treeChildren)
         {
             list = this.props.treeChildren.map((tree,index) => {
-                return  <RootDelegation 
-                    delegation = {tree.delegation}
-                    indentLevel = {this.props.indentLevel}
-                />})
+                return  <RootDelegation  delegation = {tree.delegation} />})
         }
         
         return (
-            <div style={{paddingLeft:indentLevel* Styles.indentPadding}}>
+            <div>
                 {list}
             </div>
         )
