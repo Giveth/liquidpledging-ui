5a5fd3e48236595687e97a89941350c209c92d21
Xavi <xavings@gmail.com>
2017-12-09 18:04:30 +0100

    Cloning MyFunds

--- src/OthersFunds.js
+++ src/OthersFunds.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import LPState from "./LiquidPledgingState.js"
 import GiverCard from './GiverCard'
+import FlatButton from 'material-ui/FlatButton'
+import { Styles, Currency, Merge } from './Styles'
+import Caller from './LiquidPledgingCaller'
 
-class MyProjects extends Component {
+class OthersFunds extends Component {
 
     constructor(){
         super()
 
         this.state={
-            projectNodes:[],
-            currentAddress:''
+            giverNodes:[],
+            currentAddress:'',
+            totalAmount:0,
         }
 
         LPState.on(LPState.STATE_CHANGED, this.onStateChanged)
@@ -29,55 +33,97 @@ class MyProjects extends Component {
         this.setDelegations()
     }
 
-    setDelegations=()=>
-    {
+    setDelegations=()=>{
         let currentAddress = LPState.getCurrentAccount()
-        let myGiversFilter = {adminAddress:currentAddress, type:'Project'}
-        let projectNodes = LPState.getNodes(myGiversFilter)
-
+        let myGiversFilter = {adminAddress:currentAddress, type:'Giver'}
+        let giverNodes = LPState.getNodes(myGiversFilter)
+        this.populateCards(giverNodes)
         this.setState({
-            projectNodes:projectNodes,
-            currentAddress:currentAddress
+            currentAddress:currentAddress,
         })
     }
 
-    createProjectCards=()=>
+    onNewGiver=()=>
+    {
+        let giverType = 1
+        let data = {defaultAdminType:giverType}
+        Caller.showAddAdminDialog(data)
+    }
+
+    populateCards=(giverNodes)=>
     {
         let cards = []
-        for(let projectNode of this.state.projectNodes)
+        let totalGiverAmount = 0
+        for(let giverNode of giverNodes)
         {
-            let delegations = LPState.getDelegations(projectNode.delegationsOut)
             let onlyDelegationsWithMoneyFilter = { assignedAmount:undefined}
-            let delegatesChildren = LPState.getDelegationsTrees(delegations, onlyDelegationsWithMoneyFilter)
-
             let onlyProjectsFilter= {type:'Project'}
-            let projectDelegations = LPState.getDelegationsFromTreeChildren(delegatesChildren, onlyProjectsFilter)
 
-            let projectsChildren = LPState.getDelegationsTrees(projectDelegations)
+            let delegationsIn = LPState.getDelegations(giverNode.delegationsIn) 
+
+            let delegationsOut = LPState.getDelegations(giverNode.delegationsOut) //Delegation. To  
+            let childrenOut = LPState.getDelegationsTrees(delegationsOut, onlyDelegationsWithMoneyFilter)// Children. To. No money
+
+            let delegationsToProject = LPState.getDelegationsFromTreeChildren(childrenOut, onlyProjectsFilter) //Delegations. Any level. Projects
+
+            //let projectsChildren = LPState.getDelegationsTrees(assignedToProjectsDelegations)//Children. Any level. Projects
+
+            let assignedToProjectsAmount = LPState.getNodeAssignedToProjectsAmount(giverNode) 
+            let delegatedAmount = LPState.getNodeDelegatedAmount(giverNode) - assignedToProjectsAmount
+            let availableAmount = LPState.getNodeAssignedAmount(giverNode)
+    
+            let totalAmount = availableAmount + delegatedAmount + assignedToProjectsAmount
+            totalGiverAmount += totalAmount
 
             let card = <GiverCard
-                key={projectNode.id}
-                giverNode = {projectNode}
-                delegatedDelegations={delegations}
-                delegatesChildren={delegatesChildren}
-                projectsChildren={projectsChildren}
-                userAddress={this.state.currentAddress}/>
+                key={giverNode.id}
+                giverNode = {giverNode}
+                userAddress={this.state.currentAddress}
+
+                delegationsIn={delegationsIn}
+                delegationsOut={delegationsOut}
+                delegationsToProject={delegationsToProject}
+                
+                availableAmount= {availableAmount}
+                delegatedAmount = {delegatedAmount}
+                assignedToProjectsAmount ={assignedToProjectsAmount}
+                totalAmount = {totalAmount}
+                />
 
             cards.push(card)
         }
-        return cards
+
+        this.setState({
+            cards:cards,
+            totalAmount:totalGiverAmount,
+        })
     }
 
     render() {
-
-        let cards = this.createProjectCards()
+        
+        let totalAmountText = 'Total '+ Currency.symbol+Currency.format(Currency.toEther(this.state.totalAmount))
 
         return  (
             <div >
-                 {cards}
+
+                <div style={Styles.row}>
+                    <div style = {Styles.sectionFrontCell}>
+
+                    </div>
+
+                    <div style = {Merge(Styles.sectionMiddleCell, Styles.sectionTitle)}>
+                        {totalAmountText}
+                    </div>
+
+                    <div style = {Styles.sectionBackCell}>
+                        <FlatButton onClick = {this.onNewGiver} primary = {true} label="New Giver"  />
+                    </div>
+                </div>
+
+                {this.state.cards}
+
             </div>
         )
-
     }
 }
-export default MyProjects
+export default OthersFunds
