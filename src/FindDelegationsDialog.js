27fa2c7e5ce6f170720ad9e93f6ab47301d37604
Xavi <xavings@gmail.com>
2018-01-07 10:36:40 +0100

    Setting up FindDelegations dialog

---
+++ src/FindDelegationsDialog.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+
+
+class FindDelegationsDialog extends React.Component
+{
+    constructor(props)
+    {
+        super();
+
+    }
+
+    onCancel=()=>
+    {
+        this.props.onCancel()
+    } 
+    
+    render()
+    {
+        const actions = [
+            <FlatButton
+                label="Close"
+                primary={true}
+                onClick={this.onCancel}
+            />
+        ]
+
+        let title = "Pledges for "+this.props.data.title
+       
+        let tables = []
+       
+        
+        return (
+            <Dialog
+                title={title}
+                actions={actions}
+                modal={false}
+                open={this.props.open}
+                onRequestClose={this.onCancel}
+                contentStyle={{maxWidth:'none'}}>    
+
+                
+
+            </Dialog>
+        )
+    }
+}
+
+export default FindDelegationsDialog;
