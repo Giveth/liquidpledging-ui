e11f9f1e775d7f8b33a5d3da3eac3359b49a78e4
Xavi <xavings@gmail.com>
2018-02-26 20:36:05 +0100

    Maping admin ids to names in the pledge tables

--- src/PledgesTable.js
+++ src/PledgesTable.js
@@ -2,75 +2,106 @@ import React from "react"
 import ReactTable from "react-table"
 import "react-table/react-table.css"
 import {Currency} from './Styles'
+import LPState from "./LiquidPledgingState.js"
 
 class PledgesTable extends React.Component {
 
-  render() {
+    getColorByAdminType(type)
+    {
+        if(type==="Project")
+            return "red"
+        if(type==="Delegate")
+            return "green"
+        if(type==="Giver")
+            return "yellow"
+        
+    }
 
-    let rows = 1
-    if(this.props.pledges.length)
-        rows = this.props.pledges.length
+    humanizeDelegates=(delegates)=>
+    {
+        
+        let list = delegates.map((adminId)=>{
+            let delegation = LPState.getAdmin(adminId)
+            console.log(adminId, delegation)
+            let color = this.getColorByAdminType(delegation.type)
+            console.log(color)
+            return <span 
+                style = {{color:color}}
+                key = {delegation.id}>
+                    {delegation.name}
+                </span>
+        })
+        return (<div>
+                    {list}
+                </div>)
+    }
 
-    return (        
-      <div>
-        <ReactTable
-          data={this.props.pledges}
-          columns={[
-            {
-              Header: this.props.title,
-              columns: [
-                {
-                    Header: "Id",
-                    amount: "Id",
-                    accessor: "id"
-                },
-                {
-                    Header: "Amount",
-                    id: "amount",
-                    accessor: d => d.amount,
-                    Cell: row => Currency.format(Currency.toEther(row.row.amount))
-                },
-                {
-                    Header: "Delegates",
-                    id: "delegates",
-                    accessor: d => d.delegates,
-                    Cell: row => row.row.delegates.toString()
-                },
-                {
-                    Header: "Owner",
-                    id: "owner",
-                    accessor: d => d.owner,
-                },
-                {
-                    Header: "Intended project",
-                    id: "intendedProject",
-                    accessor: d => d.intendedProject,
-                },
-                {
-                    Header: "Commit time",
-                    id: "commmitTime",
-                    accessor: d => d.commmitTime,
-                },
-                {
-                    Header: "Old pledge",
-                    id: "oldPledge",
-                    accessor: d => d.oldPledge,
-                },
-                {
-                    Header: "Payment state",
-                    id: "paymentState",
-                    accessor: d => d.paymentState,
-                },
-              ]
-            }
-          ]}
-            pageSize={rows}
-            showPaginationTop={false}
-            showPaginationBottom={false}
-            noDataText=""
-        />
+    render() {
 
-      </div>
+        let rows = 1
+        if(this.props.pledges.length)
+            rows = this.props.pledges.length
+
+        return (        
+        <div>
+            <ReactTable
+            data={this.props.pledges}
+            columns={[
+                {
+                Header: this.props.title,
+                columns: [
+                    {
+                        Header: "Id",
+                        amount: "Id",
+                        accessor: "id"
+                    },
+                    {
+                        Header: "Amount",
+                        id: "amount",
+                        accessor: d => d.amount,
+                        Cell: row => Currency.format(Currency.toEther(row.row.amount))
+                    },
+                    {
+                        Header: "Delegates",
+                        id: "delegates",
+                        accessor: d => d.delegates,
+                        Cell: row => this.humanizeDelegates(row.row.delegates)
+                    },
+                    {
+                        Header: "Owner",
+                        id: "owner",
+                        accessor: d =>d.owner + " " + LPState.getAdmin(d.owner).name,
+                    },
+                    {
+                        Header: "Intended project",
+                        id: "intendedProject",
+                        accessor: d => d.intendedProject,
+                    },
+                    {
+                        Header: "Commit time",
+                        id: "commmitTime",
+                        accessor: d => d.commmitTime,
+                    },
+                    {
+                        Header: "Old pledge",
+                        id: "oldPledge",
+                        accessor: d => d.oldPledge,
+                    },
+                    {
+                        Header: "Payment state",
+                        id: "paymentState",
+                        accessor: d => d.paymentState,
+                    },
+                ]
+                }
+            ]}
+                pageSize={rows}
+                showPaginationTop={false}
+                showPaginationBottom={false}
+                noDataText=""
+            />
+
+        </div>
     )
   }
 }
