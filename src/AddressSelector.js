2d9477340fb5dec83e68a23fe93958be82311618
Xavi <xavings@gmail.com>
2018-02-27 18:12:20 +0100

    Adapting Address selector for mergeaccounts functionality

--- src/AddressSelector.js
+++ src/AddressSelector.js
@@ -15,41 +15,58 @@ class AddressSelector extends React.Component
         }
 
         LPState.on(LPState.ACCOUNT_CHANGED, this.onAccountChanged)
+        LPState.on(LPState.MERGED_ACCOUNTS_CHANGED, this.onMergedAccountsChanged)
     }
 
     onAccountChanged=()=>
     {
+        this.setAccounts()
+    }
+
+    onMergedAccountsChanged=()=>
+    {
+        this.setAccounts()
+    }
+
+    setAccounts=()=>
+    {
         let accounts = LPState.getAccounts()
+        let isMergedAccounts = LPState.getIsMergedAccounts()
+
         if(accounts.length>1)
             accounts.push("*")
 
+        let index = LPState.getCurrentAccountIndex()
+
+        if(isMergedAccounts)
+            index = "*"
+
+        //console.log(index, isMergedAccounts, accounts)
         this.setState({
-            accounts:LPState.getAccounts(),
-            selectedIndex:LPState.getCurrentAccountIndex()
+            accounts:accounts,
+            selectedIndex:index
         })
     }
 
     onSelected = (event, index, newSelection) =>
     {
-        LPState.setAccount(newSelection)
+        LPState.setCurrentAccount(newSelection)
     }   
    
     render()
     {
-        let list = this.state.accounts.map((account, index,array)=>{
+        //console.log(this.state.selectedIndex, this.state.accounts)
+
+        let list = this.state.accounts.map((account, index, array)=>{
 
             let label = index +' - '+account
-            let key = index
-            if(account ==="*")
-            {
-                key = "*"
-                label = "Merge all the accounts ("+(array.length-1)+")"
-            }
-                
-            return <MenuItem
+            if(account =="*")
+                label = "Display as merged accounts ("+(array.length-1)+")"
+            
 
-                key= {key}
-                value={key}
+            return <MenuItem
+                key= {index}
+                value={index}
                 style={{fontSize:'0.8em'}}
                 primaryText={label} />
         })
@@ -61,7 +78,6 @@ class AddressSelector extends React.Component
                     onChange={this.onSelected}
                     style={{fontSize:'0.8em'}}
                     autoWidth={true}>
-
 
                     {list}
 
