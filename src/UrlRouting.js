018bb028da9e01a4665e874f33b59e4d3d4a5f30
Xavi <xavings@gmail.com>
2018-03-08 18:06:06 +0100

    Fixing race condition, cleaning

--- src/UrlRouting.js
+++ src/UrlRouting.js
@@ -1,10 +1,5 @@
 import QueryString from 'query-string'
-import LPState from "./LiquidPledgingState.js"
-import Web3 from 'web3'
 
-const CONTRACT = "contract"
-const ACCOUNT = "account"
-const PAGE = "page"
 const HASH_PROPERTY_CHANGED = "hashPropertyChanged"
 
 class UrlRouting {
@@ -20,17 +15,13 @@ class UrlRouting {
     onHashChanged()
     {
         let newProperties = QueryString.parse(window.location.hash)
-        let changed = false
                 
         for(let id in newProperties)
         {
-            if(this.properties[id])
+            if(this.properties[id] !== newProperties[id])
             {
-                if(this.properties[id] !== newProperties[id])
-                {
-                    this.properties[id] == newProperties[id]
-                    this.callbacks[id](newProperties[id])
-                }
+                this.properties[id] == newProperties[id]
+                this.callbacks[id](newProperties[id])
             }
         }
     }
@@ -39,40 +30,24 @@ class UrlRouting {
     {
         this.callbacks[id] = onChangeCallback
 
+        //user has defined properties in url before loading
         if(this.properties[id])
+        {
             onChangeCallback(this.properties[id])
+        }
         else
+        {
+            this.properties[id] = defaultValue
             this.setProperty(id, defaultValue)
+        }
     }
 
     setProperty=(id, value)=>
     {
-        let newProperties = JSON.parse(JSON.stringify(this.properties))
-        newProperties[id] = value
-        let newHash = QueryString.stringify(newProperties)
+        this.properties[id]=value
+        let newHash = QueryString.stringify(this.properties)
         window.location.hash=newHash
     }
-
-    processProperty(name,value)
-    {
-        if(name === CONTRACT)
-        {
-            if(Web3.utils.isAddress(value))
-            {
-                LPState.setContractAddress(value)
-                LPState.setupWeb3()
-            }
-        }
-        else if(name === ACCOUNT)
-        {
-            if(Web3.utils.isAddress(value) || value === "*")
-            {
-                LPState.setCurrentAccount(value)
-            }
-        }
-
-    }
-
 }
 
 export default new UrlRouting ()
