31132374ae70bd55b0dec186c6f3d3e62ecc3f3f
Xavi Vives <xavings@gmail.com>
2018-03-10 18:16:33 +0100

    Creating base component to handle delegation events

---
+++ src/BaseDelegationsPage.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import LPState from "./LiquidPledgingState.js"
+import Caller from './LiquidPledgingCaller'
+
+class BaseDelegationsPage extends Component {
+
+    constructor(){
+        super()
+    }
+
+    componentWillMount()
+    {
+        LPState.on(LPState.STATE_CHANGED, this.onStateChanged)
+        LPState.on(LPState.ACCOUNT_CHANGED, this.onAccountChanged)
+        LPState.on(LPState.NETWORK_CHANGED, this.onNetworkChanged)
+        LPState.on(LPState.MERGED_ACCOUNTS_CHANGED, this.onMergedAccountsChanged)
+    }
+
+    componentWillUnmount()
+    {
+        LPState.removeListener(LPState.STATE_CHANGED, this.onStateChanged)
+        LPState.removeListener(LPState.ACCOUNT_CHANGED, this.onAccountChanged)
+        LPState.removeListener(LPState.NETWORK_CHANGED, this.onNetworkChanged)
+        LPState.removeListener(LPState.MERGED_ACCOUNTS_CHANGED, this.onMergedAccountsChanged)
+    }
+
+    onStateChanged=()=>{
+        this.setDelegations()
+    }
+
+    onAccountChanged=()=>{
+        this.setDelegations()
+    }
+
+    onMergedAccountsChanged=()=>{
+        this.setDelegations()
+    }
+
+    onNetworkChanged=()=>{
+        this.setDelegations()
+    }
+
+    setDelegations=()=>{
+       console.warn("Override me!")
+    }
+
+    render() {
+
+        return  <div/>
+        
+    }
+}
+export default BaseDelegationsPage
