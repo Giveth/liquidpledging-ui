17319f0bf48de6e415d6e6e99a74b6d51f4be56a
Xavi Vives <xavings@gmail.com>
2018-03-15 15:39:05 +0100

    var renaming

--- src/Notifications.js
+++ src/Notifications.js
@@ -1,56 +1,60 @@
 import React, { Component } from 'react'
 import Caller from './LiquidPledgingCaller'
 import Snackbar from 'material-ui/Snackbar'
-
+import {Styles, Icons} from './Styles.js'
 
 class Notifications extends Component {
 
     constructor()
     {
         super()
+
         Caller.on(Caller.SHOW_NOTIFICATION, this.showNotification)
         Caller.on(Caller.CLOSE_NOTIFICATION, this.closeNotifcation)
 
         this.state={
-            snackbarOpen:false,
-            snackbarMessage:'',
-            snackBarTime:6000,
+            open:false,
+            message:'',
+            time:60000,
         }
     }
 
     showNotification=(data)=> {
+        console.error(data)
 
         this.setState({
-            snackbarMessage:data.message,
-            snackbarTime:5000,
-            snackbarOpen:true
+            message:data.message,
+            open:true
         })
     }
 
     closeNotifcation=(data)=> {
 
         this.setState({
-            snackbarOpen:false,
-            snackbarMessage:"",
-            snackBarTime:0,
+            open:false,
+            message:"",
+            time:0,
         } )
-
-      }
+    }
   
     render() {
+        let content = <div>  
+                {this.state.message}
+             </div>
+
       return (
         <div>
             <Snackbar
-                    open={this.state.snackbarOpen}
-                    message={this.state.snackbarMessage}
-                    action="Ok"
-                    autoHideDuration={this.state.snackBarTime}
-                    onActionTouchTap={this.closeSnackbar}
-                    onRequestClose={this.closeSnackbar}
-                    />
+                open={this.state.open}
+                message={content}
+                //action="Ok"
+                autoHideDuration={this.state.time}
+                onActionTouchTap={this.close} 
+                onRequestClose={this.close}
+                />
         </div>
         )
-    }
+    }   
 }
 
 export default Notifications
