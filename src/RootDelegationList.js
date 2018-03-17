64cadace0ca019345730d860746c47ff777e6720
Xavi Vives <xavings@gmail.com>
2018-03-17 10:35:34 +0100

    RootDelegations refactoring

--- src/RootDelegationList.js
+++ src/RootDelegationList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Styles, Icons, Merge, Time } from './Styles'
 import IconButton from 'material-ui/IconButton'
-import FlatButton from 'material-ui/FlatButton'
+import Button from './Button'
 import Caller from './LiquidPledgingCaller'
 import Funds from './Funds'
 import Filter from './Filter'
@@ -24,98 +24,45 @@ class RootDelegationList extends Component {
                 that.props.onPledges(d)
             }
 
-            let canCancel = true
-            let canDelegate = false
-    
-            if(d.type==="Project")
-                canDelegate = false
-    
-            let cancelDelegateButton = <div style = {Styles.emptyButton} />
-            let delegateFundsButton = <div style = {Styles.emptyButton} />
-
             let onCancel=()=>
             {
                 let data = {delegation:d, node:this.props.node}
                 Caller.showCancelDialog(data)
             }
+            
+            let actionButons =[]
     
-            if(canDelegate){
-                delegateFundsButton = (
-                <IconButton
-                    onClick = {this.onAddButton}
-                    style = {{color:'grey'}}
-                    tooltip = {'Delegate funds to '+d.name}
-                    >
-                    <Icons.add size={15}/>
-                </IconButton>)
-            }
-            
-            if(canCancel){
-                cancelDelegateButton=(
-                //icon button
-                /*<IconButton
-                    onClick = {this.onCancel}
-                    style = {{color:'grey'}}
-                    tooltip = {'Cancel funding to '+d.name}
-                    >
-                    <Icons.cancel size={15}/>
-                </IconButton>)*/
-                //text button
-                <FlatButton
-                    onClick = {onCancel}
-                    secondary = {false}
-                    label={'Cancel'}
-                    labelStyle = {{fontSize:11}}
-                />)
-            }
-            
-
-            let amount = d.assignedAmount - d.assignedToProjectsAmount
-
-            let timeLeft = <div/>
             if(d.type==="Project")
             {
-                timeLeft = <p>{Time.humanizeTimeLeft(d.commitTime) }</p>
+                let timeLeft = <div style = {{paddingLeft:10, paddingRight:10}}>{Time.humanizeTimeLeft(d.commitTime)}</div>
+                actionButons.push(timeLeft)
             }
-
-            let actionButons =(
-                <div style = {Styles.delegation.actionButons}>
-                    {timeLeft}
-                    {cancelDelegateButton}
-                    {delegateFundsButton}
-                    
-                </div>)
+            
+            let cancelDelegateButton =(<Button
+                key = "CancelButton"
+                onClick = {onCancel}
+                label='Cancel'
+                icon ={ <Icons.cancel/>}
+            />)
+        
+            actionButons.push(cancelDelegateButton)
+            
+            let amount = d.assignedAmount - d.assignedToProjectsAmount
 
             return (
 
                 <div key= {"Delegation" + index} style = {Merge(Styles.card.row, Styles.card.delegation)} >
                     <div onClick = {onPledges} >{d.name}</div>
                     <div style = {Styles.row}>
-                        {actionButons}
+
+                    <div style = {Styles.row}>
+                        {actionButons}      
+                    </div>
                         <div style={Styles.space}/>
                         <Funds amount = {amount}/>
                     </div>
                 </div>
-            )})
-                /*
-            <SectionHeader
-                key = {'Delegation'+index}
-                title={d.name}
-                titleStyle =  {Merge(d.title, {paddingLeft:10})}
-                amount= {amount}
-                amountStyle = {Merge(Styles.delegation.title, {fontSeize:'0.8em'})}
-                onPledges = {onPledges}
-                showOnHovering = {true}
-                >
-
-                {actionButons}
-                {timeLeft}
-                
-            </SectionHeader>)
-            
-            */
-                
-        
+            )})     
         
         return (
             <div>
