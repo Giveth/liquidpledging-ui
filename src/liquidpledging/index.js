8895deef91e8ff1ba9e4166cb828affb84bbe8de
Xavi Vives <xavings@gmail.com>
2018-04-15 17:16:35 +0200

    Copying over lp module bc on its current setup can't be deployed (HACK)

---
+++ src/liquidpledging/index.js
@@ -0,0 +1,5 @@
+
+exports.LiquidPledging = require('./js/liquidPledging.js');
+exports.LiquidPledgingMock = require('./js/liquidPledgingMock.js');
+exports.LiquidPledgingState = require('./js/liquidPledgingState.js');
+exports.Vault = require('./js/vault.js');
